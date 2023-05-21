e7650518e3de81caae9d4db4265ef0b932a8dddb
AtayErgun <atayergun29@gmail.com>
2023-05-21 14:36:14 +0300

    KSP

--- KafeSiparişProgramı/Form1.Designer.cs
+++ KafeSiparişProgramı/Form1.Designer.cs
@@ -29,131 +29,21 @@ namespace KafeSiparişProgramı
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(504, 76);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(272, 245);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(541, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(204, 44);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Listele";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 99);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "icecek";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 40);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "miktar";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(139, 37);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 8;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(139, 96);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 9;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(179, 171);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Ekle";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(179, 224);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Sil";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(179, 275);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Güncelle";
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
--- KafeSiparişProgramı/Form1.cs
+++ KafeSiparişProgramı/Form1.cs
@@ -7,11 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FireSharp;
-using FireSharp.Config;
-using FireSharp.Response;
-using FireSharp.Interfaces;
-using Newtonsoft.Json;
+
 
 namespace KafeSiparişProgramı
 {
@@ -21,104 +17,14 @@ namespace KafeSiparişProgramı
         {
             InitializeComponent();
         }
-        IFirebaseConfig fc = new FirebaseConfig()
-        {
-            AuthSecret = "d472QwT9xIpo2Jd5zdEVLCpw5M6DvgDPtOLO47cw",
-            BasePath = "https://console.firebase.google.com/u/0/project/kafesiparisprogrami2/database/kafesiparisprogrami2-default-rtdb/data/~2F"
-        };
-        IFirebaseClient client;
+        
        
-        /*
-        private void cayarti_btn_Click(object sender, EventArgs e)
-        {
-            int caysayisi = Convert.ToInt32(caysayisi_label.Text);
-            caysayisi++;
-            caysayisi_label.Text = Convert.ToString(caysayisi);
-        }
-
-        private void kahvearti_btn_Click(object sender, EventArgs e)
-        {
-            int kahvesayisi = Convert.ToInt32(kahvesayisi_label.Text);
-              kahvesayisi++;
-            kahvesayisi_label.Text = Convert.ToString(kahvesayisi);
-        }
-
-        private void kolaarti_btn_Click(object sender, EventArgs e)
-        {
-            int kolasayisi = Convert.ToInt32(kolasayisi_label.Text);
-            kolasayisi++;
-            kolasayisi_label.Text = Convert.ToString(kolasayisi);
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void cayeksi_btn_Click(object sender, EventArgs e)
-        {
-            int caysayisi = Convert.ToInt32(caysayisi_label.Text);
-            if (caysayisi > 0)
-            {
-                caysayisi--;
-                caysayisi_label.Text = Convert.ToString(caysayisi);
-            }
-        }
-
-        private void kahveeksi_btn_Click(object sender, EventArgs e)
-        {
-            int kahvesayisi = Convert.ToInt32(kahvesayisi_label.Text);
-            if (kahvesayisi > 0)
-            {
-                kahvesayisi--;
-                kahvesayisi_label.Text = Convert.ToString(kahvesayisi);
-            }
-        }
-
-        private void kolaeksi_btn_Click(object sender, EventArgs e)
-        {
-            int kolasayisi = Convert.ToInt32(kolasayisi_label.Text);
-            if (kolasayisi > 0)
-            {
-                kolasayisi--;
-                kolasayisi_label.Text = Convert.ToString(kolasayisi);
-            }
-        }
-
-        private void sıfırla_btn_Click(object sender, EventArgs e)
-        {
-            caysayisi_label.Text="0";
-            kolasayisi_label.Text = "0";
-            kahvesayisi_label.Text = "0";
-        }
-
-        private void siparişitamamla_btn_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Sipariş Özeti:"+" "+ caysayisi_label.Text +" "+ "Çay" +" "+ kahvesayisi_label.Text +" "+ "Kahve" +" "+ kolasayisi_label.Text +" "+ "Kola");
-        }*/
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            try
-            {
-                client = new FireSharp.FirebaseClient(fc);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Veri Tabanına Bağlantı Hatası");
-         
-            }
+            
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            sipariscls siparis = new sipariscls()
-            {
-            miktar = textBox2.Text,
-            icecek=textBox3.Text
-            };
-            var setet = client.Set("siparistbl/"+textBox2.Text,siparis);
-            MessageBox.Show("siparisler eklendi");
-        }
+        
     }
 }
